48ec8e3ffa22ea8bbf85be0774d3c75562048b23
JuliaWittler <61202570+JuliaWittler@users.noreply.github.com>
2020-03-18 17:56:37 +0100

    edit dropdown menus

--- main.js
+++ main.js
@@ -1,16 +1,5 @@
 // JavaScript Document
-const name = document.getElementById ("location"). submit ();
-const form = document.getElementById ("form")
 
-form.addEventListener ("submit", (e)=> {
-	let messages = []
-	if (location.value ==="" || name.value == null) {
-		messages.push ("Location is required")
-	}
-	
-	if (messages.length > 0){
-		e.preventDefault ()
-		errorElement.innerText = messages.join (",")
-	}
 
-})
+
+
