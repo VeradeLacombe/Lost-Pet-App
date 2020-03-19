4657c4505aeae6cf05d0698c9c38acdb3d96d3e6
Vera de Lacombe <61185476+VeradeLacombe@users.noreply.github.com>
2020-03-19 10:41:19 +0100

    Revert "Revert "Revert "Make Pet class importable"""
    This reverts commit e339e415a592cb594e61cf09fb3cc50872cdf1bf.

--- 4FoundPetList.js
+++ 4FoundPetList.js
@@ -1,4 +1,49 @@
-import Pet from "./Pet.js"
+/* eslint-env es6 */
+
+class Pet {
+	constructor(image, name, type, description, email) {
+		this.image = image;
+		this.name = name;
+		this.type = type; 
+		this.description = description; 
+		this.email = email; 
+	}
+	
+	createPoster() {
+		// Create image container
+		var imageContainer = document.createElement("div");
+		imageContainer.className = "imageContainer";
+		var image = document.createElement("img");
+		image.className = "image";
+		image.src = this.image;
+		imageContainer.appendChild(image);
+		
+		// Create name container
+		var nameContainer = document.createElement("p");
+		nameContainer.className = "nameContainer";
+		nameContainer.innerHTML = this.name;
+		
+		// Create type container
+		var typeContainer = document.createElement("p");
+		typeContainer.className = "typeContainer";
+		typeContainer.innerHTML = this.type;
+		
+		// Create description container
+		var descriptionContainer = document.createElement("p");
+		descriptionContainer.className = "descriptionContainer";
+		descriptionContainer.innerHTML = this.description;
+		
+		// Create poster, which contains the containers made above
+		var poster = document.createElement("div");
+		poster.className = "poster";
+		poster.appendChild(imageContainer);
+		poster.appendChild(nameContainer);
+		poster.appendChild(typeContainer);
+		poster.appendChild(descriptionContainer);
+		
+		return poster; 
+	}
+}
 
 const lostPets = [
 	new Pet("Labradoodle.jpg", "Coco", "Dog", "Choclate brown dog, labradoodle, she has a black collar", "email@email"),
